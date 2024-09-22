0236e51b426ca679e2b2138f6357624b64a1a425
massimiliano-demiri <massimilianodemiridev@proton.me>
2024-09-22 18:33:43 +0200

    lumina3

---
+++ src/app/book-details/BookDetailsComponent.js
@@ -0,0 +1,255 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import Lottie from "react-lottie";
+import AmazonIcon from "@mui/icons-material/ShoppingCart"; // Icona Amazon
+import ArrowBackIcon from "@mui/icons-material/ArrowBack"; // Icona Freccia per il pulsante
+import bookLoadingAnimation from "./alien.json"; // Animazione per il caricamento
+
+const BookDetailsComponent = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  // Otteniamo i valori dalla query string
+  const title = searchParams.get("title");
+  const author = searchParams.get("author");
+  const description = searchParams.get("description");
+  const coverImage = searchParams.get("cover_src");
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  // Simulazione di un tempo di caricamento per la copertina
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoaded(true), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Funzione per tornare al componente precedente
+  const goBack = () => {
+    const genres = searchParams.get("genres"); // Recupera i generi dalla query
+    const ids = searchParams.get("ids"); // Recupera gli ID dalla query
+
+    // Decodifica la stringa degli ID
+    const decodedIds = decodeURIComponent(ids);
+
+    setTimeout(() => {
+      if (genres) {
+        router.push(
+          `/nextComponent?genres=${encodeURIComponent(
+            genres
+          )}&ids=${decodedIds}`
+        );
+      } else {
+        router.push(`/nextComponent?ids=${decodedIds}`);
+      }
+    }, 300); // Ritardo per sincronizzare l'animazione
+  };
+
+  return (
+    <div
+      style={{
+        padding: "2rem",
+        backgroundColor: "#121212",
+        color: "#fff",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
+      {/* Sfondo dinamico minimalista con sfere */}
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          zIndex: -1,
+          background: "linear-gradient(135deg, #1d1d1d, #333)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+        }}
+      >
+        <div
+          style={{
+            width: "500px",
+            height: "500px",
+            backgroundColor: "#FF5722",
+            borderRadius: "50%",
+            position: "absolute",
+            top: "10%",
+            left: "-20%",
+            transform: "rotate(45deg)",
+            animation: "spin 15s linear infinite",
+            boxShadow: "0px 0px 50px rgba(255, 87, 34, 0.4)",
+          }}
+        />
+        <div
+          style={{
+            width: "400px",
+            height: "400px",
+            backgroundColor: "#FF9800",
+            borderRadius: "50%",
+            position: "absolute",
+            bottom: "15%",
+            right: "-15%",
+            animation: "spin-reverse 10s linear infinite",
+            boxShadow: "0px 0px 50px rgba(255, 152, 0, 0.3)",
+          }}
+        />
+      </div>
+
+      {isLoaded ? (
+        <>
+          {/* Copertina del libro ingrandita */}
+          {coverImage ? (
+            <img
+              src={coverImage}
+              alt="Copertina del libro"
+              style={{
+                borderRadius: "15px",
+                maxWidth: "300px",
+                maxHeight: "400px",
+                boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.6)",
+                transition: "transform 0.5s ease-in-out",
+                transform: isLoaded ? "scale(1)" : "scale(0.8)",
+              }}
+            />
+          ) : (
+            <div
+              style={{
+                width: "300px",
+                height: "400px",
+                borderRadius: "15px",
+                backgroundColor: "#333",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "#fff",
+                fontFamily: "'Noto Serif', serif",
+                boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.5)",
+                padding: "10px",
+                textAlign: "center",
+              }}
+            >
+              <h2 style={{ fontSize: "1.8rem", marginBottom: "0.5rem" }}>
+                {title}
+              </h2>
+              <h3 style={{ fontSize: "1.2rem", fontStyle: "italic" }}>
+                di {author || "Autore Sconosciuto"}
+              </h3>
+              <div
+                style={{
+                  width: "80%",
+                  height: "5px",
+                  backgroundColor: "#FF9800",
+                  margin: "1rem 0",
+                }}
+              ></div>
+              <p style={{ fontSize: "1rem" }}>
+                Esplora questa fantastica storia e lasciati coinvolgere!
+              </p>
+            </div>
+          )}
+
+          {/* Titolo e autore sotto la copertina */}
+          <div style={{ textAlign: "center", marginTop: "1.5rem" }}>
+            <h2 style={{ fontSize: "2rem", fontWeight: "bold" }}>{title}</h2>
+            <h3
+              style={{
+                fontSize: "1.5rem",
+                fontStyle: "italic",
+                color: "#FF9800",
+              }}
+            >
+              di {author || "Autore Sconosciuto"}
+            </h3>
+          </div>
+
+          {/* Descrizione del libro */}
+          <div
+            style={{
+              textAlign: "center",
+              marginTop: "1.5rem",
+              maxWidth: "600px",
+            }}
+          >
+            <p style={{ fontStyle: "italic", fontSize: "1.2rem" }}>
+              {description}
+            </p>
+          </div>
+
+          {/* Pulsante di acquisto su Amazon */}
+          <button
+            style={{
+              padding: "0.8rem 1.5rem",
+              backgroundColor: "#FF9900",
+              color: "#fff",
+              borderRadius: "10px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "2rem",
+              cursor: "pointer",
+              border: "none",
+              fontSize: "16px",
+              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+              transition: "background-color 0.3s ease",
+            }}
+            onClick={() => window.open("https://www.amazon.it", "_blank")}
+          >
+            <AmazonIcon style={{ marginRight: "10px" }} />
+            Acquista su Amazon
+          </button>
+
+          {/* Pulsante minimal per tornare indietro */}
+          <div
+            onClick={goBack}
+            style={{
+              position: "absolute",
+              bottom: "20px",
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              padding: "0.8rem 1.5rem",
+              borderRadius: "30px",
+              backgroundColor: "#282828",
+              color: "#fff",
+              fontSize: "1rem",
+              fontWeight: "500",
+              transition: "background-color 0.3s ease, transform 0.3s ease",
+            }}
+            onMouseEnter={(e) => (e.target.style.transform = "scale(1.05)")}
+            onMouseLeave={(e) => (e.target.style.transform = "scale(1)")}
+          >
+            <ArrowBackIcon style={{ marginRight: "8px" }} />
+            Torna agli estratti
+          </div>
+        </>
+      ) : (
+        // Animazione di caricamento
+        <Lottie
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: bookLoadingAnimation,
+            rendererSettings: { preserveAspectRatio: "xMidYMid slice" },
+          }}
+          height={200}
+          width={200}
+        />
+      )}
+    </div>
+  );
+};
+
+export default BookDetailsComponent;
---
+++ src/app/nextComponent/ExcerptComponent.js
@@ -0,0 +1,339 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import Lottie from "react-lottie";
+import AIcon from "@mui/icons-material/TextIncrease";
+import WbIncandescentIcon from "@mui/icons-material/WbIncandescent";
+import loadingAnimation from "./alien.json";
+import "./transitionStyles.css";
+
+const fetchBookAndDetails = async (bookId) => {
+  const proxyUrl = "https://thingproxy.freeboard.io/fetch/";
+  const url = `https://www.gutenberg.org/cache/epub/${bookId}/pg${bookId}.txt`;
+
+  try {
+    const response = await fetch(proxyUrl + url);
+    if (response.ok) {
+      const text = await response.text();
+      const lines = text.split("\n");
+      const title =
+        lines
+          .find((line) => line.startsWith("Title: "))
+          ?.replace("Title: ", "") || "Titolo Sconosciuto";
+      const author =
+        lines
+          .find((line) => line.startsWith("Author: "))
+          ?.replace("Author: ", "") || "Autore Sconosciuto";
+      return { text, title, author };
+    } else {
+      throw new Error(`Errore nel download del libro con ID: ${bookId}`);
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      text: null,
+      title: "Titolo Sconosciuto",
+      author: "Autore Sconosciuto",
+    };
+  }
+};
+
+const getSignificantParagraph = (text) => {
+  const startIdx = Math.floor(text.length / 8);
+  const limitedText = text.slice(startIdx);
+  const sentences = limitedText.match(/[^.!?]+[.!?]+/g);
+
+  if (!sentences || sentences.length === 0) return null;
+
+  let selectedParagraph = "";
+  let charCount = 0;
+
+  for (let i = 0; i < sentences.length; i++) {
+    const sentence = sentences[i].trim();
+    if (
+      charCount === 0 &&
+      sentence.charAt(0) !== sentence.charAt(0).toUpperCase()
+    ) {
+      continue;
+    }
+    if (charCount === 0 && sentence.length > 50) {
+      selectedParagraph += sentence + " ";
+      charCount += sentence.length;
+    } else if (charCount > 0 && charCount + sentence.length <= 1900) {
+      selectedParagraph += sentence + " ";
+      charCount += sentence.length;
+    } else {
+      break;
+    }
+  }
+
+  return selectedParagraph.trim() || null;
+};
+
+const ExcerptComponent = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const idsQuery = searchParams.get("ids");
+  const [bookData, setBookData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [fontSize, setFontSize] = useState(18);
+  const [darkMode, setDarkMode] = useState(true);
+  const [blurActive, setBlurActive] = useState(true);
+  const [bookId, setBookId] = useState(null);
+
+  const bookIds = idsQuery ? idsQuery.split(",") : [];
+
+  const lottieOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const revealBookDetails = () => {
+    if (bookData && bookId) {
+      const queryParams = new URLSearchParams({
+        title: bookData.title,
+        author: bookData.author,
+        paragraph: bookData.paragraph,
+        bookId: bookId,
+        ids: bookIds.join(","),
+      }).toString();
+
+      router.push(`/book-details?${queryParams}`);
+    }
+  };
+
+  const getRandomBookData = () => {
+    if (bookIds.length > 0) {
+      const randomIndex = Math.floor(Math.random() * bookIds.length);
+      return bookIds[randomIndex];
+    }
+    return null;
+  };
+
+  const extractBook = async (id = null) => {
+    setLoading(true);
+    setBlurActive(true);
+    setBookData(null);
+
+    let found = false;
+    let selectedBookId = id || getRandomBookData();
+
+    while (!found && selectedBookId) {
+      const { text, title, author } = await fetchBookAndDetails(selectedBookId);
+      if (text) {
+        const significantParagraph = getSignificantParagraph(text);
+        if (significantParagraph) {
+          setBookData({
+            id: selectedBookId,
+            paragraph: significantParagraph,
+            title,
+            author,
+          });
+          setBookId(selectedBookId);
+          found = true;
+        }
+      }
+      if (!id) selectedBookId = getRandomBookData();
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const idFromParams = searchParams.get("id");
+    extractBook(idFromParams);
+  }, [idsQuery]);
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const adjustFontSize = (increase) => {
+    setFontSize((prevSize) => (increase ? prevSize + 2 : prevSize - 2));
+  };
+
+  const renderBlurredText = (text) => {
+    const sentences = text.split(" ");
+    const visibleSentences = sentences
+      .slice(0, Math.floor(sentences.length * 0.8))
+      .join(" ");
+    const blurredSentences = sentences
+      .slice(Math.floor(sentences.length * 0.8))
+      .join(" ");
+
+    return (
+      <p style={{ fontSize: `${fontSize}px`, textAlign: "justify" }}>
+        {visibleSentences}{" "}
+        <span
+          style={{
+            filter: blurActive ? "blur(5px)" : "none",
+            transition: "filter 0.5s ease-in-out",
+            backgroundColor: blurActive ? "rgba(0, 0, 0, 0.2)" : "transparent",
+            padding: "2px 4px",
+            borderRadius: "4px",
+          }}
+        >
+          {blurredSentences}
+        </span>
+      </p>
+    );
+  };
+
+  return (
+    <div
+      style={{
+        padding: "1rem",
+        backgroundColor: darkMode ? "#121212" : "#f0f0f0",
+        color: darkMode ? "#fff" : "#000",
+        minHeight: "100vh",
+        transition: "background-color 0.5s, color 0.5s",
+        fontFamily: "'Noto Serif', serif",
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "100vw",
+      }}
+    >
+      {/* Sezione con le icone in alto */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "15px",
+          marginBottom: "0.5rem",
+        }}
+      >
+        <button
+          onClick={() => adjustFontSize(true)}
+          style={{
+            cursor: "pointer",
+            backgroundColor: "transparent",
+            border: "none",
+          }}
+        >
+          <AIcon
+            fontSize="large"
+            style={{ color: darkMode ? "#fff" : "#000" }}
+          />
+        </button>
+        <button
+          onClick={() => adjustFontSize(false)}
+          style={{
+            cursor: "pointer",
+            backgroundColor: "transparent",
+            border: "none",
+          }}
+        >
+          <AIcon
+            fontSize="large"
+            style={{ color: darkMode ? "#fff" : "#000" }}
+          />
+        </button>
+        <button
+          onClick={toggleTheme}
+          style={{
+            cursor: "pointer",
+            backgroundColor: "transparent",
+            border: "none",
+            transition: "transform 0.5s ease",
+            transform: darkMode ? "rotate(0deg)" : "rotate(180deg)",
+          }}
+        >
+          <WbIncandescentIcon
+            fontSize="large"
+            style={{ color: darkMode ? "#fff" : "#000" }}
+          />
+        </button>
+      </div>
+
+      {loading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "60vh",
+          }}
+        >
+          <Lottie options={lottieOptions} height={200} width={200} />
+        </div>
+      ) : bookData ? (
+        <>
+          <div
+            style={{
+              flex: 1,
+              overflowY: "auto",
+              padding: "0.5rem",
+              marginBottom: "60px",
+              maxHeight: "calc(100vh - 130px)",
+              overflow: "hidden",
+            }}
+          >
+            {renderBlurredText(bookData.paragraph)}
+          </div>
+
+          {/* Pulsanti in fondo */}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "30px",
+              position: "fixed",
+              bottom: "20px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              zIndex: 1,
+            }}
+          >
+            <button
+              onClick={() => extractBook()}
+              style={{
+                padding: "1rem",
+                backgroundColor: "#FF9800",
+                color: "#fff",
+                borderRadius: "50%",
+                cursor: "pointer",
+                border: "none",
+                fontSize: "24px",
+                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+                transition: "transform 0.3s ease",
+              }}
+              onMouseEnter={(e) => (e.target.style.transform = "scale(1.1)")}
+              onMouseLeave={(e) => (e.target.style.transform = "scale(1)")}
+            >
+              🤮
+            </button>
+
+            <button
+              onClick={revealBookDetails}
+              style={{
+                padding: "1rem",
+                backgroundColor: "#FF1744",
+                color: "#fff",
+                borderRadius: "50%",
+                cursor: "pointer",
+                border: "none",
+                fontSize: "24px",
+                boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+                transition: "transform 0.3s ease",
+              }}
+              onMouseEnter={(e) => (e.target.style.transform = "scale(1.1)")}
+              onMouseLeave={(e) => (e.target.style.transform = "scale(1)")}
+            >
+              ❤️
+            </button>
+          </div>
+        </>
+      ) : (
+        <p>Nessun estratto disponibile.</p>
+      )}
+    </div>
+  );
+};
+
+export default ExcerptComponent;
